4684d75ceb6aa75d2ac6e2468cdd8aca581ec148
jihyun923 <152951869+jihyun923@users.noreply.github.com>
2024-03-16 13:19:35 +0000

    과제_03

--- hw03/04.area_input3.py
+++ hw03/04.area_input3.py
@@ -3,4 +3,4 @@ upper = int(input("사다리꼴의 윗변의 길이를 입력하세요."))
 height = int(input("사다리꼴의 높이를 입력하세요."))
 
 area = (bottom + upper) * height / 2
-print("밑변의 길이가 {}이고 윗변의 길이가 {}, 높이가 {}인 사다리꼴의 면적은 {}입니다.".format(bottom,upper,height,area))
+print("밑변의 길이가 {}cm이고 윗변의 길이가 {}cm, 높이가 {}cm인 사다리꼴의 면적은 {}입니다.".format(bottom,upper,height,area))
