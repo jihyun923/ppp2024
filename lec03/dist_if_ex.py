5f1e5bbc279ada3fca5198e593fc5f6a1d9dad6c
jihyun923 <152951869+jihyun923@users.noreply.github.com>
2024-03-18 06:45:50 +0000

    3주차 수업 코드

--- lec03/dist_if_ex.py
+++ lec03/dist_if_ex.py
@@ -3,11 +3,13 @@ import math
 x1 = 0
 y1 = 0
 x2 = 3
-y2 = 4
+y2 = 5
 
 dist = math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
 if dist > 1 :
-    print("거리는 {}입니다.".format(dist))
+    print("거리는 {:.1f}입니다.".format(dist))
 else :
     print("거리가 너무 가깝습니다.")
+#:.1f 원하는 자리수만큼 출력
+#:.숫자f
