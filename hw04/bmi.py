6583f24c265cd93a7fa2f600edcd9d389c1f5b4e
jihyun923 <152951869+jihyun923@users.noreply.github.com>
2024-03-20 05:41:29 +0000

    과제_04

--- hw04/bmi.py
+++ hw04/bmi.py
@@ -2,7 +2,10 @@ height = int(input("키를 입력하세요."))
 weight = int(input("몸무게를 입력하세요."))
 bmi = weight / (height/100)**2
 
-if 23 <= bmi <= 24.9 :
+#좌항과 우항의 조건에 모두 만족해야 하기 때문에 and 필수
+if bmi < 23:
+    print("비만이 아닙니다.")
+elif 23 <= bmi and bmi <= 24.9 :
     print("키 {}cm, 몸무게 {}kg에 따른 bmi는 {}kg/㎡입니다. 비만 전단계입니다.".format(height,weight,bmi))
 elif bmi <= 29.9 :
     print("키 {}cm, 몸무게 {}kg에 따른 bmi는 {}kg/㎡입니다. 1단계 비만입니다.".format(height,weight,bmi))
