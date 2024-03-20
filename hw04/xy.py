baf7cf179de6e137106d02434649f45c09878eee
jihyun923 <152951869+jihyun923@users.noreply.github.com>
2024-03-20 05:23:06 +0000

    과제_04

--- hw04/xy.py
+++ hw04/xy.py
@@ -1,18 +1,17 @@
 x = int(input("x의 좌표값을 입력하세요."))
 y = int(input("y의 좌표값을 입력하세요."))
 
-if x > 0 and y > 0 :
+if x > 0 and y > 0:
     print("({},{}), 입력한 좌표는 제1사분면입니다.".format(x,y))
-elif x < 0 and y > 0 :
+elif x < 0 and y > 0:
     print("({},{}), 입력한 좌표는 제2사분면입니다.".format(x,y))
-elif x < 0 and y < 0 :
+elif x < 0 and y < 0:
     print("({},{}), 입력한 좌표는 제3사분면입니다.".format(x,y))
-elif x > 0 and y < 0 :
+elif x > 0 and y < 0:
     print("({},{}), 입력한 좌표는 제4사분면입니다.".format(x,y))
-elif x == 0 and y == 0 :
+elif x == 0 and y == 0:
     print("(0,0), 입력한 좌표는 원점입니다.")
-elif x == 0 :
+elif x == 0 and (y > 0 or y < 0):
     print("입력한 좌표는 y축 위에 있습니다.")
-elif y == 0 :
+elif y == 0 and (x > 0 or x < 0):
     print("입력한 좌표는 x축 위에 있습니다.")
-    quit()
