8810fdc8f4f64efabdacfcd0c14ca1fc18c93977
jihyun923 <152951869+jihyun923@users.noreply.github.com>
2024-03-21 03:48:28 +0000

    과제_05

--- hw05/sct.py
+++ hw05/sct.py
@@ -6,7 +6,7 @@ for i in range(0, 361, 15):
     s = round(math.sin(r), 4)
     c = round(math.cos(r), 4)
     if i % 90 == 0:
-        t = "        Undefined"
+        t = "Undefined"
     else:
         t = round(math.tan(r), 4)
     print(f"{i}\t\t{s}\t\t{c}\t\t{t}")
