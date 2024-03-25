43401663ec2f58cd6dec8239ca580fdb5d000ac5
jihyun923 <152951869+jihyun923@users.noreply.github.com>
2024-03-25 06:04:46 +0000

    과제_05

--- hw05/gugudan.py
+++ hw05/gugudan.py
@@ -1,4 +1,6 @@
 dan = int(input("원하는 구구단 단수를 입력하세요."))
 
 for i in range(1,10):
-    print(f"{dan} * {i} = {dan * (i)}")
+    print(f"{dan} * {i} = {dan * i:2d}")
+#i+=1 누적이나 합계에 관한 식에 쓸 수 있음
+#i+1 = n으로 써서 치환해서 사용 가능
